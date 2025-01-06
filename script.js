700dd877c10613939607c07fe6f14a53d29f406d
JyotiradeetiBagal <139766154+JyotiradeetiBagal@users.noreply.github.com>
2025-01-06 17:41:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 function volume_sphere() {
-    //Write your code here
-  
-} 
+    let radius = parseFloat(document.getElementById("radius").value);
+    let volumeElement = document.getElementById("volume");
+    
+    if (isNaN(radius) || radius < 0) {
+        volumeElement.value = "NaN";
+		return;
+    }
+    const volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
+    const roundedVolume = volume.toFixed(4);
+    volumeElement.value = roundedVolume;
+}
 
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+window.onload = document.getElementById('MyForm').onsubmit = function(event) {
+    event.preventDefault();
+    volume_sphere();
+};
